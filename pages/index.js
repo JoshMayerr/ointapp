5d17a627b6b7fa495287a0dc3d992ffcaf0563d0
Josh Mayer <josh.25.mayer@gmail.com>
2021-09-24 11:34:48 -0700

    sghop

--- pages/index.js
+++ pages/index.js
@@ -178,12 +178,11 @@ export default function Home() {
                     the one step setup.
                   </p>{" "}
                   <div className="inline-flex rounded-full shadow mt-6 ">
-                    <a
-                      href="#"
-                      className="inline-flex items-center px-4 py-1 border border-transparent text-base tracking-wider font-medium rounded-full text-white bg-indigo-500 hover:bg-indigo-400 transition duration-200 ease-in-out"
-                    >
-                      Buy Now
-                    </a>
+                    <Link href="https://shop.oint.app">
+                      <a className="inline-flex items-center px-4 py-1 border border-transparent text-base tracking-wider font-medium rounded-full text-white bg-indigo-500 hover:bg-indigo-400 transition duration-200 ease-in-out">
+                        Buy Now
+                      </a>
+                    </Link>
                   </div>
                 </div>
               </div>
