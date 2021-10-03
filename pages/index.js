6856585043bf9b6ebe45c095bbec49e78f707bc5
Josh Mayer <josh.25.mayer@gmail.com>
2021-10-02 23:47:20 -0700

    wrka

--- pages/index.js
+++ pages/index.js
@@ -93,14 +93,8 @@ export default function Home() {
                     </h1>
 
                     <Link href="https://instagram.com/ointapp" target="_blank">
-                      <a className="mt-6 -mr-1 inline-flex items-center px-3 py-2 text-lg leading-none font-medium transition duration-200 ease-in-out text-indigo-500 border-b-2 hover:border-indigo-500 border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Want one for free? Follow and DM us on Instagram
-                        <div className="">
-                          <ArrowSmRightIcon
-                            className="ml-1 -mr-0.5 h-5 w-5 "
-                            aria-hidden="true"
-                          />
-                        </div>
+                      <a className="mt-6 -mr-1 inline-flex items-center px-3 py-2 text-lg leading-none font-medium transition duration-200 ease-in-out text-indigo-500  hover:text-indigo-400  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Want one for free? Follow and DM us on Instagram 🠚
                       </a>
                     </Link>
                   </FadeIn>
